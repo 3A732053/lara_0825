347a03ca6707b149df82457891819f001b4378dd
Lin Yu Lun <wuyahayaa01@gmail.com>
2020-11-10 18:58:29 +0800

    查詢資料(all、find、where)

--- routes/web.php
+++ routes/web.php
@@ -33,10 +33,85 @@ $post -> content = 'test content';
 $post -> save();
 */
 
+
 #create方式新增
+/*
 Post::create
 ([
     'title'=>'test title',
     'content'=>'text content',
 ]);
+
+Post::create
+([
+    'title'=>'test title2',
+    'content'=>'text content2',
+]);
+
+Post::create
+([
+    'title'=>'test title3',
+    'content'=>'text content3',
+]);
+
+Post::create
+([
+    'title'=>'test title4',
+    'content'=>'text content4',
+]);
+
+Post::create
+([
+    'title'=>'test title5',
+    'content'=>'text content5',
+]);
+
+Post::create
+([
+    'title'=>'test title6',
+    'content'=>'text content6',
+]);
+
+Post::create
+([
+    'title'=>'test title7',
+    'content'=>'text content7',
+]);
+
+Post::create
+([
+    'title'=>'test title8',
+    'content'=>'text content8',
+]);
+
+Post::create
+([
+    'title'=>'test title9',
+    'content'=>'text content9',
+]);
+
+Post::create
+([
+    'title'=>'test title10',
+    'content'=>'text content10',
+]);
+*/
+
+#all(),將所有資料印出
+/*
+$posts = Post::all();
+dd($posts);
+*/
+
+#find(),找出特定資料
+/*
+$post = Post::find(1);
+dd($post);
+*/
+
+#where(資料欄位,條件判斷式,資料內容),找出特定範圍資料
+/*
+$posts = Post::where('id','<',10)->orderBy('id','DESC')->get();
+dd($posts);
+*/
 
